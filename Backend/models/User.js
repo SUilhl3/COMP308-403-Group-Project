32d4e673830d04c47ea1c33139c76b1b16d3bcca
Tajinder <tnijjar2@my.centennialcollege.ca>
2026-04-04 19:41:31 -0400

    Add password hashing to backend

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,10 +1,18 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
     userID: {type: String, requried: true},
     username: {type: String, required: true, unique: true},
     password: {type: String, required: true},
     email: {type: String, required: true}
+});
+
+//password hashing
+userSchema.pre("save", async function () {
+    const salt = await bcrypt.genSalt(12);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
 })
 
 export const User = mongoose.model("User", userSchema);
