122e4f0d46c30517380abaeeb820dde378cf4d67
Tajinder <tnijjar2@my.centennialcollege.ca>
2026-04-04 22:47:51 -0400

    Add ApolloProvider to main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,8 +3,24 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client"
+import {ApolloProvider} from "@apollo/client"
+
+const link = createHttpLink({
+  uri: 'http://localhost:4000/graphql',
+  credentials: 'include'
+})
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link
+})
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ApolloProvider client = {client}>
+      <App />
+    </ApolloProvider>
   </StrictMode>,
 )
