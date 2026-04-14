046df05ac2c756f8079f9c681d5c2606a9a530b9
SUilhl3 <ilagang@my.centennialcollege.ca>
2026-04-13 21:59:37 -0400

    sure

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -4,12 +4,14 @@ import { useUser } from "../Hooks/useUser";
 function Home()
 {
     return (
-        <div>
+        <div class = "flex items-center justify-center">
+         <div class = "w-full max-w-lg bg-gradient-to-r from-blue-200 to-cyan-200 p-20 rounded-lg shadow-lg">
             <h1>Home</h1>
             <p> Welcome to the page! Create a backlog list for your video games here!</p>
             <i>Want to get started?</i> <Link to="/register" class="font-medium text-fg-brand hover:underline">Create an Account</Link>
             <br></br>
             <i>Already have an account?</i> <Link to="/login" class="font-medium text-fg-brand hover:underline">Login</Link>
+        </div>
         </div>
     )
 }
